3d013a28ff0895ab6b185745610c6ed4a2d00964
Зюзин Никита <launman@mail.ru>
2026-06-01 13:03:06 +0300

    Update ST-5

--- src/test/java/com/mycompany/app/SqrtTest.java
+++ src/test/java/com/mycompany/app/SqrtTest.java
@@ -9,68 +9,62 @@ import org.junit.jupiter.api.Test;
 class SqrtTest {
 
     @Test
-    void averageOfTwoPositiveNumbers() {
-        Sqrt sqrt = new Sqrt(4.0);
-        assertEquals(2.0, sqrt.average(1.0, 3.0), 1e-12);
+    void averageOfSixAndFour() {
+        Sqrt sqrt = new Sqrt(16.0);
+        assertEquals(5.0, sqrt.average(6.0, 4.0), 1e-12);
     }
 
     @Test
-    void averageOfNegativeAndPositive() {
-        Sqrt sqrt = new Sqrt(4.0);
-        assertEquals(0.0, sqrt.average(-2.0, 2.0), 1e-12);
+    void averageOfTenAndTwenty() {
+        Sqrt sqrt = new Sqrt(16.0);
+        assertEquals(15.0, sqrt.average(10.0, 20.0), 1e-12);
     }
 
     @Test
-    void goodReturnsTrueForExactSquareRoot() {
-        Sqrt sqrt = new Sqrt(9.0);
-        assertTrue(sqrt.good(3.0, 9.0));
+    void goodIsTrueForExactSqrtOfSixteen() {
+        Sqrt sqrt = new Sqrt(16.0);
+        assertTrue(sqrt.good(4.0, 16.0));
     }
 
     @Test
-    void goodReturnsFalseForBadGuess() {
-        Sqrt sqrt = new Sqrt(9.0);
-        assertFalse(sqrt.good(2.0, 9.0));
+    void goodIsFalseForWrongGuessOfSixteen() {
+        Sqrt sqrt = new Sqrt(16.0);
+        assertFalse(sqrt.good(5.0, 16.0));
     }
 
     @Test
-    void improveReturnsExpectedNewtonStep() {
-        Sqrt sqrt = new Sqrt(2.0);
-        assertEquals(1.5, sqrt.improve(1.0, 2.0), 1e-12);
+    void improveFromTwoForEight() {
+        Sqrt sqrt = new Sqrt(8.0);
+        assertEquals(3.0, sqrt.improve(2.0, 8.0), 1e-12);
     }
 
     @Test
-    void improveKeepsPerfectGuessStable() {
-        Sqrt sqrt = new Sqrt(9.0);
-        assertEquals(3.0, sqrt.improve(3.0, 9.0), 1e-12);
+    void improveKeepsExactGuessForTwentyFive() {
+        Sqrt sqrt = new Sqrt(25.0);
+        assertEquals(5.0, sqrt.improve(5.0, 25.0), 1e-12);
     }
 
     @Test
-    void iterConvergesForTwo() {
-        Sqrt sqrt = new Sqrt(2.0);
-        assertEquals(Math.sqrt(2.0), sqrt.iter(1.0, 2.0), 1e-7);
+    void iterConvergesForTwentyFive() {
+        Sqrt sqrt = new Sqrt(25.0);
+        assertEquals(5.0, sqrt.iter(1.0, 25.0), 1e-7);
     }
 
     @Test
-    void iterConvergesForLargeValue() {
-        Sqrt sqrt = new Sqrt(10_000.0);
-        assertEquals(100.0, sqrt.iter(1.0, 10_000.0), 1e-6);
+    void iterConvergesForFortyNine() {
+        Sqrt sqrt = new Sqrt(49.0);
+        assertEquals(7.0, sqrt.iter(1.0, 49.0), 1e-7);
     }
 
     @Test
-    void calcReturnsSqrtForTwo() {
-        Sqrt sqrt = new Sqrt(2.0);
-        assertEquals(Math.sqrt(2.0), sqrt.calc(), 1e-7);
+    void calcSqrtOfSixteen() {
+        Sqrt sqrt = new Sqrt(16.0);
+        assertEquals(4.0, sqrt.calc(), 1e-7);
     }
 
     @Test
-    void calcReturnsSqrtForFractionalValue() {
-        Sqrt sqrt = new Sqrt(0.25);
-        assertEquals(0.5, sqrt.calc(), 1e-7);
-    }
-
-    @Test
-    void calcReturnsSqrtForOne() {
-        Sqrt sqrt = new Sqrt(1.0);
-        assertEquals(1.0, sqrt.calc(), 1e-12);
+    void calcSqrtOfEightyOne() {
+        Sqrt sqrt = new Sqrt(81.0);
+        assertEquals(9.0, sqrt.calc(), 1e-7);
     }
 }
